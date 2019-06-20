8ee7be87a05cea6072add558119def8a695dc35b
Charles Jiang <charles.jiangbl@gmail.com>
2019-06-20 22:30:05 +1200

    Initial commit

---
+++ Models/Input.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace testapi.Models
+{
+    public class Input
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ Models/Output.cs
@@ -0,0 +1,11 @@
+namespace testapi.Models
+{
+    public class Output
+    {
+        public double Gst { get; set; }
+
+        public double TotalExcludingGst { get; set; }
+
+        public string CostCentre { get; set; }
+    }
+}
